457bd797455bb1cf2bb0d3f29daebba51f50e71c
Carlos Loaeza <carlosloaeza100598@gmail.com>
2025-09-03 11:44:59 -0600

    fix customer

--- fullstack/src/app/customer.ts
+++ fullstack/src/app/customer.ts
@@ -3,7 +3,7 @@ import { Signal} from '@angular/core';
 export class Customer {
 
   public constructor(
-    public id: Signal<string | undefined>,
+    public id: Signal<number>,
     public name: Signal<string>,
     public email: Signal<string>,
   ) { }
